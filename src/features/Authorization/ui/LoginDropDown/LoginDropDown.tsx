6f849675f505da88af36e9824b467256922be87e
rgdzv <r_gadzhiomarov@mail.ru>
2025-04-22 19:05:42 +0300

    refactor: change logindropdown configuration

--- src/features/Authorization/ui/LoginDropDown/LoginDropDown.tsx
+++ src/features/Authorization/ui/LoginDropDown/LoginDropDown.tsx
@@ -1,15 +1,10 @@
-import { DropDown } from 'shared/ui'
+import { CustomButton, DropDown } from 'shared/ui'
 import { ProfileIcon } from 'shared/assets'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import type { FC } from 'react'
 
 export const LoginDropDown: FC = () => {
-    const triggerContent = (
-        <>
-            <ProfileIcon title='Профиль' />
-            <span>Профиль</span>
-        </>
-    )
+    const [authorized] = useState(true)
 
     const handleLogOut = () => {
         console.log('Вы вышли из системы!')
@@ -36,11 +31,25 @@ export const LoginDropDown: FC = () => {
         ]
     }, [])
 
-    return (
+    const triggerContent = (
+        <>
+            <ProfileIcon title='Профиль' />
+            <span>Профиль</span>
+        </>
+    )
+
+    const buttonCondition = authorized ? (
         <DropDown
             triggerContent={triggerContent}
             buttonClassName='primary'
             options={options}
         />
+    ) : (
+        <CustomButton className='primary'>
+            <ProfileIcon title='Профиль' />
+            <span>Войти</span>
+        </CustomButton>
     )
+
+    return buttonCondition
 }
