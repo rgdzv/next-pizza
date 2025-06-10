1861b12bcd41756622a69504dcadaa9e1eaf3283
rgdzv <r_gadzhiomarov@mail.ru>
2025-06-10 19:29:33 +0300

    refactor: add border to arrow buttons inside popover

--- src/features/SortPizzas/ui/SortPizzas.tsx
+++ src/features/SortPizzas/ui/SortPizzas.tsx
@@ -5,28 +5,30 @@ import { SORTLIST } from '../lib/const/sortList'
 import type { FC } from 'react'
 
 export const SortPizzas: FC = () => {
-    const [name] = useState('рейтингу')
+    const [obj, setObj] = useState({ name: 'рейтингу', sortProperty: 'rating' })
 
     const triggerContent = (
         <>
             <ArrowsUpDownIcon title='Сортировка' />
             <span>Сортировка по:</span>
-            <span>{name}</span>
+            <span>{obj.name}</span>
         </>
     )
 
-    const sortOptions = SORTLIST.map((sortName) => {
+    const sortOptions = SORTLIST.map((item) => {
+        const handleClick = () => {
+            setObj({ ...item })
+        }
+
         return (
-            <li key={sortName.name}>
-                <div style={{ display: 'flex', gap: '5px' }}>
-                    <CustomButton className='sortArrow'>
-                        <ArrowUpIcon title='По возрастанию' />
-                    </CustomButton>
-                    <CustomButton className='sortArrow'>
-                        <ArrowDownIcon title='По убыванию' />
-                    </CustomButton>
-                </div>
-                <span>{sortName.name}</span>
+            <li key={item.name}>
+                <CustomButton className='sortArrow' onClick={handleClick}>
+                    <ArrowUpIcon title='По возрастанию' />
+                </CustomButton>
+                <CustomButton className='sortArrow' onClick={handleClick}>
+                    <ArrowDownIcon title='По убыванию' />
+                </CustomButton>
+                <span>{item.name}</span>
             </li>
         )
     })
