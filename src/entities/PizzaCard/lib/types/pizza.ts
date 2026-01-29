6c1b9bd029e3d8018f52527b4dbb8f9722c2f794
rgdzv <r_gadzhiomarov@mail.ru>
2026-01-29 19:53:29 +0300

    refactor: add ingredients to db.json inside all pizzas

--- src/entities/PizzaCard/lib/types/pizza.ts
+++ src/entities/PizzaCard/lib/types/pizza.ts
@@ -1,3 +1,8 @@
+interface Ingredients {
+    name: string
+    removable: boolean
+}
+
 export interface Pizza {
     id: string
     title: string
@@ -9,4 +14,5 @@ export interface Pizza {
     description: string
     price: Record<string, number>
     weight: Record<string, Record<string, number>>
+    ingredients: Ingredients[]
 }
