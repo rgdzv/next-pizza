88612665f4bef5ea96185a8d891ea517d00f3c80
rgdzv <r_gadzhiomarov@mail.ru>
2026-09-08 19:55:59 +0300

    refactor: add all stores inside Setting.test.tsx to pass build job inside workflow

--- src/pages/Settings/ui/Settings.test.tsx
+++ src/pages/Settings/ui/Settings.test.tsx
@@ -1,9 +1,26 @@
 import { render, screen } from '@testing-library/react'
+import { BasketPizzaStoreProvider } from 'features/Pizzas/BasketPizzas'
+import { PizzasStoreProvider } from 'features/Pizzas/AllPizzas'
+import { ChosenPizzaStoreProvider } from 'features/Pizzas/ChosenPizza'
 import Settings from './Settings'
+
+window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn()
+}))
 
 describe('Settings', () => {
     test('render', () => {
-        render(<Settings />)
+        render(
+            <PizzasStoreProvider>
+                <ChosenPizzaStoreProvider>
+                    <BasketPizzaStoreProvider>
+                        <Settings />
+                    </BasketPizzaStoreProvider>
+                </ChosenPizzaStoreProvider>
+            </PizzasStoreProvider>
+        )
 
         const settings = screen.getByTestId('settings')
         expect(settings).toBeInTheDocument()
